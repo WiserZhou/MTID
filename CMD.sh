dc10b2f89f1100f0dfa705cb8aaad2357ec56837
WiserZhou <WiserZYF@gmail.com>
2024-07-29 19:16:21 +0800

    dataset

--- CMD.sh
+++ CMD.sh
@@ -2,4 +2,9 @@ nohup python main_distributed.py --dataset=crosstask_how --name=new_interpolatio
       --base_model=predictor > out/output_new_interpolation3.log 2>&1 &
 
 
-nohup python train_mlp.py --name=note --dataset=crosstask_base --gpu=3 > out/output_note.log 2>&1 &
+nohup python train_mlp.py --name=note3 --dataset=crosstask_base --gpu=1 > out/output_note3.log 2>&1 &
+nohup python train_mlp.py --name=note4 --dataset=crosstask_base --gpu=2 > out/output_note4.log 2>&1 &
+nohup python train_mlp.py --name=note5 --dataset=crosstask_base --gpu=3 > out/output_note5.log 2>&1 &
+nohup python train_mlp.py --name=note6 --dataset=crosstask_base --gpu=1 > out/output_note6.log 2>&1 &
+nohup python train_mlp.py --name=note7 --dataset=crosstask_base --gpu=2 > out/output_note7.log 2>&1 &
+nohup python train_mlp.py --name=note8 --dataset=crosstask_base --gpu=3 > out/output_note8.log 2>&1 &
